54b2acaa138a0a7d4132e2f893c140a3321be695
dipita-tasnim <tasnim.dipita@gmail.com>
2025-02-15 11:32:43 +0600

    setup fixed!

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get("/products", (req, res) => {
     // res.send("server is ready");
 });
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
     connectDB();
